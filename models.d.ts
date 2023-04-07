a6843e1a2851f62a69047e89686a09bc734daaa0
Domey Benjamin <domeybenjamin1@gmail.com>
2023-04-07 22:21:18 +0000

    added formik and modeled solutions

---
+++ models.d.ts
@@ -0,0 +1,35 @@
+import { BlockTypes } from './types';
+
+export interface MachineType {
+  /** Unique ID of each machine type configured. */
+  id: string;
+  /** Name of machine type. */
+  name: string;
+  /** Unqiue for each machine type. */
+  slug: string;
+  /** Attributes the machine type possesses. */
+  attributes: Array<MachineTypeAttribute>;
+  /** Meta data for machine type.  */
+  metaData: MachineTypeMetaData;
+}
+
+interface MachineTypeAttribute {
+  /** Name of attribute. */
+  name: string;
+  /** Type of data to expect for this attribute. */
+  type: BlockTypes;
+}
+
+interface MachineTypeMetaData {
+  /** Title attribute for each machine created. */
+  titleAttribute: string;
+}
+
+interface Machine {
+  /** Unique ID of each machine created. */
+  id: string;
+  /** Machine Type Id. */
+  machineTypeId: string;
+  /** Data holds all information user enters for that machine. */
+  data: Record<string, string | number | Date | boolean>;
+}
--- source/components/AddMachineType/index.tsx
+++ source/components/AddMachineType/index.tsx
@@ -2,46 +2,97 @@ import { Box, Button, InfoIcon, Input, Pressable, Text, Center, Modal, Flex, Ico
 import { AttributeDialog } from "../AttributeDialog"
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { DeleteAttributeDialog } from "../DeleteAttributeDialog";
+import * as Yup from 'yup';
+import { Formik } from "formik";
+import { BlockTypes } from "../../../types";
 
 interface Props {
     isOpen: boolean
     close: VoidFunction
 }
 
+interface AttributesProps {
+    name: string;
+    type: BlockTypes
+}
+
+interface FormFieldsParams {
+    name: string;
+    attributes: Array<AttributesProps>
+}
+
+const validationSchema = Yup.object({
+    name: Yup.string().required("Name of Required"),
+    attributes: Yup.array(
+        Yup.object({
+            name: Yup.string().required("Name of Required"),
+            type: Yup.string().oneOf(['TEXT', 'NUMBER', 'CHECKBOX', 'DATE']).required("Name of Required"),
+        })
+    )
+});
+
 const attributes = [1]
 export const AddMachineType = ({ isOpen, close }: Props) => {
+
+    const handleSubmit = (values: FormFieldsParams) => {
+        console.log(values)
+    }
+
     return (
-        <Modal isOpen={isOpen} onClose={close} size='xl'>
-            <Modal.Content h="full">
-                <Modal.CloseButton />
-                <Modal.Header>Add New Machine Type</Modal.Header>
-                <Modal.Body>
-                    <Text mb={1}>Name *</Text>
-                    <Input size='2xl' />
-                    <Box mt={5}>
-                        <Text mb={1} color='gray.400'>ATTRIBUTES</Text>
-                        {
-                            attributes.length ? (
-                                <>
-                                    <AttributeDialog type='Add' InvokeAttributeComponent={({ onPress }) => (
-                                        <AddNewAttribute onPress={onPress} />
-                                    )} />
-                                    <Text mt={4} fontSize='xs' color='gray.400'>NB: Click on an attribute to make it the title attribute.</Text>
-                                    <Attribute active={true} />
-                                    <Attribute />
-                                    <Attribute />
-                                </>
-                            ) : <NoAttributesAdded />
-                        }
-                    </Box>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onPress={close} width='full'>
-                        Add
-                    </Button>
-                </Modal.Footer>
-            </Modal.Content>
-        </Modal>
+        <Formik
+            initialValues={{ name: '', attributes: [] }}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+        >
+            {
+                ({ handleChange, handleBlur, handleSubmit, values, errors, handleReset }) => (
+                    <Modal
+                        isOpen={isOpen}
+                        onClose={() => {
+                            handleReset();
+                            close();
+                        }}
+                        size='xl'>
+                        <Modal.Content h="full">
+                            <Modal.CloseButton />
+                            <Modal.Header>Add New Machine Type</Modal.Header>
+                            <Modal.Body>
+                                <Text mb={1}>Name *</Text>
+                                <Input size='2xl' isInvalid={Boolean(errors.name)}
+                                    onChangeText={handleChange('name')}
+                                    onBlur={handleBlur('name')}
+                                    value={values.name}
+                                />
+                                {
+                                    errors.name ? <Text fontSize="xs" color='red.600'>Name of Required</Text> : null
+                                }
+                                <Box mt={5}>
+                                    <Text mb={1} color='gray.400'>ATTRIBUTES</Text>
+                                    {
+                                        attributes.length ? (
+                                            <>
+                                                <AttributeDialog type='Add' InvokeAttributeComponent={({ onPress }) => (
+                                                    <AddNewAttribute onPress={onPress} />
+                                                )} />
+                                                <Text mt={4} fontSize='xs' color='gray.400'>NB: Click on an attribute to make it the title attribute.</Text>
+                                                <Attribute active={true} />
+                                                <Attribute />
+                                                <Attribute />
+                                            </>
+                                        ) : <NoAttributesAdded />
+                                    }
+                                </Box>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button onPress={() => handleSubmit()} width='full'>
+                                    Add
+                                </Button>
+                            </Modal.Footer>
+                        </Modal.Content>
+                    </Modal>
+                )
+            }
+        </Formik>
     )
 }
 
