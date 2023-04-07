635af4e239216d37ef29346de7111e1dbefd0cbc
Domey Benjamin <domeybenjamin1@gmail.com>
2023-04-07 15:49:07 +0000

    added machine listings

---
+++ source/components/DeleteMachineTypeDialog/index.tsx
@@ -0,0 +1,39 @@
+import { Box, Button, Flex, InfoIcon, Input, Pressable, Stack, Text, Icon, Center, Modal, Radio, AlertDialog, IconButton } from "native-base"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
+import { RFValue } from "react-native-responsive-fontsize"
+import { useDisclosure } from "../../../hooks/useDisclosure"
+import { useRef } from "react"
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+interface Props {
+}
+
+export const DeleteMachineTypeDialog = ({ }: Props) => {
+    const { isOpen, open, close } = useDisclosure()
+    const cancelRef = useRef(null);
+
+    return (
+        <>
+            <IconButton variant='subtle' onPress={open} ml={2} backgroundColor='red.600' icon={<Icon as={Ionicons} size='sm' name='trash-outline' color='white' />} />
+            <AlertDialog leastDestructiveRef={cancelRef} isOpen={isOpen} onClose={close}>
+                <AlertDialog.Content>
+                    <AlertDialog.CloseButton />
+                    <AlertDialog.Header>Delete Machine Type</AlertDialog.Header>
+                    <AlertDialog.Body>
+                        You are deleting this machine type, <Text fontWeight='bold'>Budozer.</Text>
+                    </AlertDialog.Body>
+                    <AlertDialog.Footer>
+                        <Button.Group space={2}>
+                            <Button variant="unstyled" colorScheme="coolGray" onPress={close} ref={cancelRef}>
+                                Cancel
+                            </Button>
+                            <Button colorScheme="danger" onPress={close}>
+                                Delete
+                            </Button>
+                        </Button.Group>
+                    </AlertDialog.Footer>
+                </AlertDialog.Content>
+            </AlertDialog>
+        </>
+    )
+}
--- source/screens/configurations/index.tsx
+++ source/screens/configurations/index.tsx
@@ -1,37 +1,74 @@
-import { AddIcon, Box, Button, Center, Fab, InfoIcon, Text } from "native-base"
+import { AddIcon, Badge, Box, Button, Center, Fab, Flex, Icon, IconButton, InfoIcon, Pressable, ScrollView, Text } from "native-base"
 import { RFValue } from "react-native-responsive-fontsize"
 import { useDisclosure } from "../../../hooks/useDisclosure"
 import { AddMachineType } from "../../components/AddMachineType"
-
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { SafeAreaView } from 'react-native'
+import { DeleteMachineTypeDialog } from "../../components/DeleteMachineTypeDialog";
 
 const machineTypes = [1]
 export const MachineTypesConfigurations = () => {
     const addState = useDisclosure()
 
     return (
-        <>
-            <Box style={{ flex: 1, position: 'relative' }}>
-                <Center style={{ flex: 1 }}>
-                    <InfoIcon size='10' color='gray.300' />
-                    <Box my={RFValue(10)}>
-                        <Text color='gray.600' fontSize='lg'>No Machine Type Added</Text>
-                    </Box>
-                    <Button size='lg' onPress={addState.open}>
-                        Add Machine Type
-                    </Button>
-                </Center>
-                {
-                    machineTypes.length ? (
-                        <Fab
-                            placement="bottom-right"
-                            size="lg"
-                            onPress={addState.open}
-                            icon={<AddIcon name="share" as="Entypo" />}
-                        />
-                    ) : null
-                }
-            </Box>
+        <SafeAreaView style={{ flex: 1 }}>
+            {
+                machineTypes.length ? (
+                    <ScrollView>
+                        <MachineType />
+                        <MachineType />
+                        <MachineType />
+                        <MachineType />
+                    </ScrollView>
+                ) : (
+                    <Center style={{ flex: 1 }}>
+                        <InfoIcon size='10' color='gray.300' />
+                        <Box my={RFValue(10)}>
+                            <Text color='gray.600' fontSize='lg'>No Machine Type Added</Text>
+                        </Box>
+                        <Button size='lg' onPress={addState.open}>
+                            Add Machine Type
+                        </Button>
+                    </Center>
+                )
+            }
+            {
+                machineTypes.length ? (
+                    <Fab
+                        placement="bottom-right"
+                        size="lg"
+                        onPress={addState.open}
+                        icon={<AddIcon name="share" as="Entypo" />}
+                    />
+                ) : null
+            }
             <AddMachineType close={addState.close} isOpen={addState.isOpen} />
-        </>
+        </SafeAreaView>
+    )
+}
+
+const MachineType = () => {
+    return (
+        <Box background='white' mx={3} mt={3} p={4} borderRadius='md' borderWidth={1} borderColor='gray.200'>
+            <Icon as={Ionicons} name='construct-outline' size='lg' />
+            <Box mt={3}>
+                <Text fontWeight='bold' fontSize='lg'>Hello world</Text>
+            </Box>
+            {
+                [1, 2, 3].map(() => (
+                    <Flex flexDirection='row' mt={2} alignItems='center' borderWidth={1} borderColor='gray.100' p={2} borderRadius='md' justifyContent='space-between'>
+                        <Text>Weight</Text>
+                        <Badge ml={2} colorScheme='blue'>Number</Badge>
+                    </Flex>
+                ))
+            }
+
+            <Flex alignItems='flex-end' mt={5}>
+                <Flex flexDirection='row' alignItems='center' justifyContent='space-between'>
+                    <IconButton variant='subtle' backgroundColor='blue.600' icon={<Icon as={Ionicons} name='create-outline' size='sm' color='white' />} />
+                    <DeleteMachineTypeDialog />
+                </Flex>
+            </Flex>
+        </Box>
     )
 }
